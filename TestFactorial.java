e2a9875968550cf99b49e9ab7970147454b2a1f8
teju2028 <durgatejaswanikolli@gmail.com>
2026-09-10 14:48:54 +0530

    Update TestFactorial.java

--- TestFactorial.java
+++ TestFactorial.java
@@ -1,18 +1,20 @@
-public static void main(String args[])
-{
-    try{
-        if(Factorial.compute(5)!=120)
-        {
-            throw new AssertionError("Factorial not valid");
+public class TestFactorial {
+
+    public static void main(String args[]) {
+        try {
+            if (Factorial.compute(5) != 120) {
+                throw new AssertionError("Factorial not valid");
+            }
+
+            if (Factorial.compute(1) != 1) {
+                throw new AssertionError("Factorial not valid");
+            }
+
+            System.out.println("Successful Factorial");
         }
-        if(Factorial.compute(1)!=5)
-        {
-            throw new AssertionError("Factorial not valid");
+        catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            System.exit(1);
         }
-        System.out.println("Successful Factorial");
-    }
-    catch(Exception e)
-    {
-        System.err.println("Exception, enter a valid one");
     }
 }
